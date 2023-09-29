f23f74d21fe6c63f9c0d96b31be675ffec1bb812
dennyabraham <dennysabraham@gmail.com>
2023-09-28 20:41:15 -0400

    wip: 4

--- problems/ruby/0004/problem_four.rb
+++ problems/ruby/0004/problem_four.rb
@@ -1,4 +1,32 @@
 # frozen_string_literal: true
 
 module ProblemFour
+  def self.palindrome?(number)
+    number.to_s == number.to_s.reverse
+  end
+
+  def three_digit_sums
+    Enumerator::Lazy.new do
+      (999...100).each do |jdx|
+        (999...100).each do |idx|
+          yield idx * jdx
+        end
+      end
+    end
+  end
+
+  def find
+    three_digit_sums.reduce(1) do |max, n|
+      if n < max
+        n
+      else if palindrome?(n)
+        max
+      else
+        n
+      end
+    end
+  end
 end
+
+
+
--- problems/ruby/0004/problem_four_spec.rb
+++ problems/ruby/0004/problem_four_spec.rb
@@ -3,4 +3,19 @@
 require_relative 'problem_four'
 
 RSpec.describe ProblemFour do
+  describe '.palindrome?' do
+    it 'knows a palindrome' do
+      expect(ProblemFour).to be_a_palindrome(1)
+      expect(ProblemFour).to be_a_palindrome(121)
+      expect(ProblemFour).to be_a_palindrome(1221)
+      expect(ProblemFour).to be_a_palindrome(420024)
+    end
+
+    it "knows a non palindrome" do
+      expect(ProblemFour).not_to be_a_palindrome(13)
+      expect(ProblemFour).not_to be_a_palindrome(123)
+      expect(ProblemFour).not_to be_a_palindrome(1231)
+      expect(ProblemFour).not_to be_a_palindrome(420069)
+    end
+  end
 end
