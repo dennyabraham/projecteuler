3ffc675be1dc38429b77fa132d1f461cc7699633
dennyabraham <dennyabraham@users.noreply.github.com>
2024-02-17 23:11:13 -0500

    wip

--- problems/ruby/0874/problem.rb
+++ problems/ruby/0874/problem.rb
@@ -3,12 +3,20 @@ class Problem
 
   class << self
     def tth_prime(n)
-      PRIMES[n] || primes(cache: true).first(n + 1).last
+      PRIMES[n] || primes.first(n + 1).last
     end
 
     def prime_score(list)
       list.sum do |elem|
         tth_prime(elem)
+      end
+    end
+
+    def all_primes_to(n)
+      if PRIMES[n + 1]
+        PRIMES.first(n + 1)
+      else
+        primes.first(n + 1)
       end
     end
 
@@ -42,23 +50,53 @@ class Problem
     end
 
     def candidate_list_lazy(base, max_len)
-      Enumerator::Lazy.new((base**max_len).downto(0)) do |yielder, idx|
-        list = Array.new(max_len) { 0 }
-
-        (max_len.downto(0)).each do |position|
-          one = base ** position
-
-          while(idx > one)
-            idx -= one
-            list[max_len - position - 1] += 1
+      # base = 2
+      Enumerator::Lazy.new(base.downto(0)) do |yielder, i|
+        list = Array.new(max_len, i)
+        # for i generate all combos with all numbers after i
+        (1..max_len).each do |position|
+          (base - 1).downto(i + 1) do |num|
+            list[position * -1] = num
+            yielder << list
           end
         end
-        yielder << list
       end
     end
 
-    def primes(cache: false, sequential: false)
-      natural_numbers { |n| prime?(n, cache: cache, sequential: sequential) }
+    def candidate_list_distributions(base, max_len)
+      number = base ** max_len
+      results = []
+      list = Array.new(base, 0)
+      list[0] = max_len
+
+      results << list.dup
+
+      # until(results.last.last == max_len)
+      7.times do
+        list = list.dup
+        only_zeroes_from_end = true
+        list.reverse_each.with_index do |elem, ridx|
+          if only_zeroes_from_end && elem == 0
+            next
+          elsif only_zeroes_from_end && ridx > 0
+            only_zeroes_from_end = false
+            list[-ridx - 1] -= 1
+            list[-ridx] += 1
+          else
+            only_zeroes_from_end = false
+            list[-ridx - 1] -= 1
+            list[-ridx] = 
+          end
+
+          results << list.dup
+        end
+      end
+
+      results
+    end
+
+    def primes
+      natural_numbers { |n| prime?(n) }
     end
 
     def natural_numbers
@@ -74,19 +112,19 @@ class Problem
       end
     end
 
-    def prime?(number, cache: false, sequential: false)
+    def prime?(number, sequential: false)
       if number <= PRIMES.last
         PRIMES.include?(number)
       elsif PRIMES.any? { |prime| (number % prime).zero? }
         false
-      elsif sequential
-        PRIMES << number if cache
+      elsif sequential # idk what this does
+        PRIMES << number
         true
       else
         prime = !(PRIMES.last...((number / 2).ceil)).any? do |factor|
           (number % factor).zero?
         end
-        PRIMES << number if prime && cache
+        PRIMES << number if prime
         prime
       end
     end
--- problems/ruby/0874/problem_spec.rb
+++ problems/ruby/0874/problem_spec.rb
@@ -26,17 +26,72 @@ describe Problem, :aggregate_failures do
       [0, 1, 1, 1, 1],
       [1, 1, 1, 1, 1],
     )
+  end
 
-    # https://www.baeldung.com/cs/generate-k-combinations
+  it 'generates all primes to x' do
+    expect(Problem.all_primes_to(3)).to eq([2, 3, 5, 7])
   end
 
   it "finds the maximum divisible prime score" do
     expect(Problem.maximal_multiple_prime_score(2, 5)).to eq(14)
   end
 
-  it "finds the max divisible prime score of two high numbers" do
+  it "returns distributions, in order, for base 2 with length 4", :focus do
+    expect(Problem.candidate_list_distributions(3, 3).to_a).to eq([
+      [3,  0,  0],
+      [2,  1,  0],
+      [2,  0,  1],
+      [1,  2,  0],
+      [1,  1,  1],
+      [1,  0,  2],
+      [0,  3,  0],
+      [0,  2,  1],
+      [0,  1,  2],
+      [0,  0,  3]])
+  end
+
+  xit "finds the max divisible prime score of two high numbers" do
     prime = Problem.tth_prime(7000)
 
     puts Problem.maximal_multiple_prime_score(7000, prime)
   end
 end
+
+=begin
+ 2 1 0
+
+ 3 0 0  6
+ 2 1 0  5 <- %k = 0
+ 2 0 1  4
+ 1 2 0  4
+ 1 1 1  3
+ 1 0 2  2
+ 0 3 0  3
+ 0 2 1  2
+ 0 1 2  1
+ 0 0 3  0
+
+
+ 5 0 0
+ 4 1 0
+ 4 0 1
+ 3 2 0
+ 3 1 1
+ 3 0 2
+ 2 3 0
+ 2 2 1
+ 2 1 2
+ 2 0 3
+ 1 4 0
+ 1 3 1
+ 1 2 2
+ 1 1 3
+ 1 0 4
+ 0 5 0
+ 0 4 1
+ 0 3 2
+ 0 2 3
+ 0 1 4
+ 0 0 5
+
+=end
